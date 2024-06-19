010f6542a84f5a085be1eb9dc143b179b86f82d2
Giovane Iwamoto <81972850+GiovaneIwamoto@users.noreply.github.com>
2024-06-19 17:38:11 -0400

    feat: serverless tts

--- api/src/handlers/description_handler.py
+++ api/src/handlers/description_handler.py
@@ -2,7 +2,7 @@ import json
 
 def v1_description(event, context):
     body = {
-        "message": "Text to Speech"
+        "message": "TTS api version 1.0"
     }
     response = {"statusCode": 200, "body": json.dumps(body)}
     return response
--- api/src/handlers/health_handler.py
+++ api/src/handlers/health_handler.py
@@ -2,7 +2,7 @@ import json
 
 def health(event, context):
     body = {
-        "message": "Serverless executed successfully!",
+        "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event,
     }
     response = {"statusCode": 200, "body": json.dumps(body)}
